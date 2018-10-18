e8f7189a8ba4782a37207085b02ac330346680aa
yohei tanaka <oceanyoh@gmail.com>
2018-10-19 00:40:39 +0900

    Make user-table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,28 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments   ('id');
+            $table->string       ('last_name')   ->comment('姓');
+            $table->string       ('first_name')  ->comment('名');
+            $table->string       ('l_n_kana')    ->comment('姓カナ');
+            $table->string       ('f_n_kana')    ->comment('名カナ');
+            $table->tinyInteger  ('gender')      ->comment('性別 0:男性, 1:女性, 2:その他');
+            $table->date         ('birthday')    ->comment('誕生日');
+            $table->string       ('zip')         ->comment('郵便番号');
+            $table->integer      ('city_id')     ->comment('市区町村ID')->unsigned();
+            $table->string       ('street')      ->comment('番地');
+            $table->string       ('building')    ->comment('建物')->nullable();
+            $table->string       ('tel_private') ->comment('個人電話');
+            $table->string       ('tel_work')    ->comment('社用電話')->nullable();
+            $table->string       ('email')       ->comment('メールアドレス')->unique();
+            $table->timestamp    ('email_verified_at')->nullable();
+            $table->string       ('password')    ->comment('パスワード');
+            $table->integer      ('code')        ->comment('社員番号')->unsigned();
+            $table->tinyInteger  ('role')        ->comment('権限')->unsigned();
+            $table->integer      ('section')     ->comment('部署')->unsigned();
+            $table->integer      ('position')    ->comment('役職')->unsigned();
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps   ();
         });
     }
 
