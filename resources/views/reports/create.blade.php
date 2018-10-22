97acc27a76b66e649e4236cb0fe3edf0998ed9e9
root <oceanyoh@gmail.com>
2018-10-23 00:06:20 +0900

    Add view files

---
+++ resources/views/reports/create.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.common')
+@section('title', '日報作成')
+@section('content')
+@endsection
---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.common')
+@section('title', '日報一覧')
+@section('content')
+@endsection
