4811aece9a4ff890b6061a31ded85609717234a4
yohei tanaka <oceanyoh@gmail.com>
2018-10-28 03:47:18 +0900

    Update header

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,20 +1,32 @@
 @php
 $user = Auth::user();
+$user = $user->last_name . $user->first_name;
+$time = date('H');
+switch ($time) {
+    case $time < 6:
+        $greeting = 'おやすみなさい…';
+        break;
+    case $time < 12:
+        $greeting = 'おはようございます！';
+        break;
+    case $time < 18:
+        $greeting = 'こんにちは！';
+        break;
+    default:
+        $greeting = 'こんばんは！';
+        break;
+}
 @endphp
 <a href="/">
-  <h1 class="left">システム</h1>
+    <h1 class="left">システム</h1>
 </a>
-@if($user)
-	<a class="menu" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-	<form id="logout-form" action="{{ route('logout') }}" method="POST">
-		@csrf
-	</form>
-	<a class="menu" href="">
-	@if($user)
-		こんにちは！{{$user->name}}さん
-	@endif
-	</a>
-@else
-	<a class="menu" href="{{ route('login') }}">ログイン</a>
-	<a class="menu" href="{{ route('register') }}">新規登録</a>
+<p>{{ date('Y/m/d') }}</p>
+<a class="menu" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+<form id="logout-form" action="{{ route('logout') }}" method="POST">
+    @csrf
+</form>
+@if ($user)
+<a class="menu" href="">
+    {{ $greeting }} {{ $user }} さん
+</a>
 @endif
